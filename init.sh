fb149f9f20af8cbd5c4a213f53a3a53620b8b29c
pipzer0 <andy.giron@protonmail.com>
2024-08-04 18:21:26 -0700

    prod

--- init.sh
+++ init.sh
@@ -6,139 +6,92 @@ update_system() {
     sudo apt-get update && sudo apt-get upgrade -y || { echo "Failed to update system. Exiting..."; exit 1; }
 }
 
-instal_no_machine() {
-    echo "Installing NoMachine"
-    wget https://download.nomachine.com/download/8.11/Linux/nomachine_8.11.3_4_amd64.deb || {echo "failed to pull NoMachine. Exiting...."; exit 1;}
-    dpkg -i nomachine_8.11.3_4_amd64.deb || { echo "failed to install No Machines. Exiting.."; exit 1;}
+install_no_machine() {
+    echo "Installing NoMachine..."
+    if ! dpkg -l | grep -q nomachine; then
+        wget https://download.nomachine.com/download/8.11/Linux/nomachine_8.11.3_4_amd64.deb || { echo "Failed to download NoMachine. Exiting..."; exit 1; }
+        sudo dpkg -i nomachine_8.11.3_4_amd64.deb || { echo "Failed to install NoMachine. Exiting..."; exit 1; }
+    else
+        echo "NoMachine is already installed. Skipping..."
+    fi
 }
 
 install_general_tools() {
     echo "Installing general malware analysis tools..."
-    sudo apt-get install -y git python3 python3-pip wireshark clamav yara chkrootkit rkhunter strace gdb || { echo "Failed to install general tools. Exiting..."; exit 1; }
-    sudo apt-get install -y tcpdump || { echo "Failed to install network analysis tools. Exiting..."; exit 1; }
+    sudo apt-get install -y git python3 python3-pip wireshark clamav yara chkrootkit rkhunter strace gdb tcpdump || { echo "Failed to install general tools. Exiting..."; exit 1; }
 }
 
 install_elf_tools() {
     echo "Installing ELF analysis tools..."
-    sudo apt-get install -y binutils elfutils ltrace patchelf binwalk || { echo "Failed to install ELF tools. Exiting..."; exit 1; }
-    sudo apt-get install -y hexedit objdump nmap htop || { echo "Failed to install additional analysis tools. Exiting..."; exit 1; }
+    sudo apt-get install -y binutils elfutils ltrace patchelf binwalk hexedit nmap htop || { echo "Failed to install ELF tools. Exiting..."; exit 1; }
 }
 
 install_static_analysis_tools() {
     echo "Installing static analysis tools..."
-    sudo apt-get install -y radare2 || { echo "Failed to install radare2. Exiting..."; exit 1; }
-    sudo snap install rizin --classic || { echo "Failed to install rizin. Exiting..."; exit 1; }
-    pip3 install pefile capstone uncompyle6 || { echo "Failed to install pip packages for static analysis. Exiting..."; exit 1; }
+    pip3 install --upgrade pefile capstone uncompyle6 || { echo "Failed to install pip packages for static analysis. Exiting..."; exit 1; }
 }
 
 install_dynamic_analysis_tools() {
     echo "Installing dynamic analysis tools..."
-    pip3 install angr || { echo "Failed to install angr. Exiting..."; exit 1; }
-    sudo apt-get install -y dynamorio || { echo "Failed to install dynamorio. Exiting..."; exit 1; }
-    sudo pip3 install frida || { echo "Failed to install frida. Exiting..."; exit 1; }
+    pip3 install --upgrade angr || { echo "Failed to install angr. Exiting..."; exit 1; }
+    sudo pip3 install --upgrade frida || { echo "Failed to install frida. Exiting..."; exit 1; }
+}
+
+install_r2() {
+    echo "Installing radare2..."
+    if [ ! -d "radare2" ]; then
+        git clone https://github.com/radareorg/radare2 || { echo "Failed to clone radare2 repository. Exiting..."; exit 1; }
+    else
+        cd radare2
+        git pull || { echo "Failed to update radare2 repository. Exiting..."; exit 1; }
+        cd ..
+    fi
+    cd radare2
+    sudo ./sys/install.sh || { echo "Failed to install radare2. Exiting..."; exit 1; }
+    cd ..
 }
 
 install_pip() {
     echo "Installing pip packages..."
-    pip3 install ROPgadget volatility3 || { echo "Failed to install pip packages. Exiting..."; exit 1; }
+    pip3 install --upgrade ROPgadget volatility3 || { echo "Failed to install pip packages. Exiting..."; exit 1; }
 }
 
-install_peda_pwndbg_gef () {
-    local installer_path=$PWD
-    if [ -f ~/.gdbinit ] || [ -h ~/.gdbinit ]; then
-        echo "[+] backing up gdbinit file"
-        cp ~/.gdbinit ~/.gdbinit.back_up
-    fi
-
-    # download peda and decide whether to overwrite if exists
-    if [ -d ~/peda ] || [ -h ~/.peda ]; then
-        echo "[-] PEDA found"
-        read -p "skip download to continue? (enter 'y' or 'n') " skip_peda
-
-        if [ $skip_peda = 'n' ]; then
-            rm -rf ~/peda
-            git clone https://github.com/longld/peda.git ~/peda
+install_gdb_plugins() {
+    echo "Installing GDB plugins..."
+    if ! command -v gdb-peda &> /dev/null; then
+        if [ ! -d "gdb-peda-pwndbg-gef" ]; then
+            git clone https://github.com/pipzer0/gdb-peda-pwndbg-gef || { echo "Failed to clone gdb-peda-pwndbg-gef repository. Exiting..."; exit 1; }
         else
-            echo "PEDA skipped"
+            cd gdb-peda-pwndbg-gef
+            git pull || { echo "Failed to update gdb-peda-pwndbg-gef repository. Exiting..."; exit 1; }
+            cd ..
         fi
+        cd gdb-peda-pwndbg-gef
+        chmod +x install.sh
+        ./install.sh || { echo "Failed to install GDB plugins. Exiting..."; exit 1; }
+        cd ..
     else
-        echo "[+] Downloading PEDA..."
-        git clone https://github.com/longld/peda.git ~/peda
+        echo "GDB PEDA is already installed. Skipping installation..."
     fi
-
-    # download pwndbg
-    if [ -d ~/pwndbg ] || [ -h ~/.pwndbg ]; then
-        echo "[-] Pwndbg found"
-        read -p "skip download to continue? (enter 'y' or 'n') " skip_pwndbg
-
-        if [ $skip_pwndbg = 'n' ]; then
-            rm -rf ~/pwndbg
-            git clone https://github.com/pwndbg/pwndbg.git ~/pwndbg
-
-            cd ~/pwndbg
-            ./setup.sh
-        else
-            echo "Pwndbg skipped"
-        fi
-    else
-        echo "[+] Downloading Pwndbg..."
-        git clone https://github.com/pwndbg/pwndbg.git ~/pwndbg
-
-        cd ~/pwndbg
-        ./setup.sh
-    fi
-
-    # download gef
-    echo "[+] Downloading GEF..."
-    git clone https://github.com/hugsy/gef.git ~/gef
-
-    cd $installer_path
-
-    echo "[+] Setting .gdbinit..."
-    cp gdbinit ~/.gdbinit
-
-    {
-      echo "[+] Creating files..."
-        sudo cp gdb-peda /usr/bin/gdb-peda &&\
-        sudo cp gdb-pwndbg /usr/bin/gdb-pwndbg &&\
-        sudo cp gdb-gef /usr/bin/gdb-gef
-    } || {
-      echo "[-] Permission denied"
-        exit
-    }
-
-    {
-      echo "[+] Setting permissions..."
-        sudo chmod +x /usr/bin/gdb-*
-    } || {
-      echo "[-] Permission denied"
-        exit
-    }
-
 }
 
 cleanup() {
     echo "Cleaning up..."
+    rm -rf nomachine_8.11.3_4_amd64.deb radare2 db-peda-pwndbg-gef || { echo "Failed to clean up. Exiting..."; exit 1; }
 }
+
 
 main() {
     update_system
-    instal_no_machine
+    install_no_machine
     install_general_tools
     install_elf_tools
     install_static_analysis_tools
     install_dynamic_analysis_tools
     install_pip
-    install_peda_pwndbg_gef
+    install_r2
+    install_gdb_plugins
     cleanup
-
-    echo "Reloading .bashrc to apply changes..."
-    source ~/.bashrc
-
-    echo "Installation of malware analysis tools complete. Run the following commands to launch the corresponding GDB environment: 
-        gdb-peda
-        gdb-pwndbg
-        gdb-gef"
 }
 
 # Run the script
